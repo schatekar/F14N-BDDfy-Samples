c4bb6fb2093ccac71addfbadcc8fa3575e7387e1
Suhas Chatekar <suhas.chatekar@collinsontech.com>
2014-06-26 18:18:02 +0100

    Very simple facebook login tests

---
+++ F14N-BDDfy-Sample/FacebookLoginPage.cs
@@ -0,0 +1,32 @@
+﻿using FluentAutomation;
+
+namespace F14N_BDDfy_Sample
+{
+    public class FacebookLoginPage : PageObject<FacebookLoginPage>
+    {
+        private const string EmailInput = "input[id='email']";
+        private const string PasswordInput = "input[id='pass']";
+        private const string LoginButton = "input[value='Log in']";
+
+        public FacebookLoginPage(FluentTest test) : base(test)
+        {
+            Url = "http://www.facebook.com";
+            At = () => I.Expect.Exists(EmailInput);
+        }
+
+        public FacebookHomePage Login(string email, string password)
+        {
+            I.Enter(email).In(EmailInput);
+            I.Enter(password).In(PasswordInput);
+            I.Click(LoginButton);
+
+            if (I.Find(LoginButton) != null) return null;
+
+            return Switch<FacebookHomePage>();
+        }
+    }
+
+    public class FacebookHomePage : PageObject<FacebookLoginPage> {
+        public FacebookHomePage(FluentTest test) : base(test) {}
+    }
+}
---
+++ F14N-BDDfy-Sample/FacebookLoginStory.cs
@@ -0,0 +1,59 @@
+﻿using FluentAutomation;
+using NUnit.Framework;
+using TestStack.BDDfy;
+using TestStack.BDDfy.Scanners.StepScanners.Fluent;
+
+namespace F14N_BDDfy_Sample
+{
+    [TestFixture]
+    public class FacebookLoginStory : FluentTest
+    {
+        private FacebookLoginPage facebookLoginPage;
+        private FacebookHomePage facebookHomePage;
+
+        public FacebookLoginStory()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+            FluentSession.EnableStickySession();
+        }
+
+        [Test]
+        public void CannotLoginToFacebookWithInvalidCrdentials()
+         {
+             new FacebookLoginStory()
+                 .Given(s => s.IAmOnFacebookLoginPage())
+                 .And(s => s.ILoginUsingEmailAndPassword("a@b.com", "password"), "I login using email '{0}' and password '{1}'")
+                 .Then(s => s.ShowsMessage("Incorrect email address"))
+                 .BDDfy();
+         }
+        [Test]
+        public void PasswordIsMandatory()
+        {
+            new FacebookLoginStory()
+                .Given(s => s.IAmOnFacebookLoginPage())
+                .And(s => s.ILoginUsingEmailAndPassword("a@b.com", ""), "I login using email '{0}' and password '{1}'")
+                .Then(s => s.ShowsMessage("Please re-enter your password"), "Error message '{0}' is displayed")
+                .BDDfy();
+        }
+
+        private void ShowsMessage(string message)
+        {
+            I.Assert.Text(message);
+        }
+
+        private void ILoginUsingEmailAndPassword(string email, string password)
+        {
+            facebookHomePage = facebookLoginPage.Login(email, password);
+        }
+
+        private void IAmOnFacebookLoginPage()
+        {
+            facebookLoginPage = new FacebookLoginPage(this).Go();
+        }
+
+        public void TearDown()
+        {
+            I.Dispose();
+        }
+    }
+}
